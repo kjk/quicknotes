330aaa125dc3e03b460228980509c8a32f3537f5
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-04 16:33:37 -0700

    more work on events logging

--- events.go
+++ events.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/kjk/u"
@@ -27,33 +28,35 @@ type EventsLog struct {
 }
 
 func reopenEventsFileIfNeeded() error {
+	var err error
 	if eventsLog.file != nil {
 		if eventsLog.currTime.YearDay() == time.Now().YearDay() {
 			return nil
 		}
 	}
-	/*
-		path := filepath.Join(dir, "events.log.json")
-		if eventsLog.file != nil {
-			eventsLog.file.Close()
-			eventsLog.file = nil
-			newPath := eventsLog.currTime.Format("events.log-2006-01-02.json")
-			err := os.Rename(path, newPath)
-			if err != nil {
-
-			}
-		}
-
-		eventsLog.currTime = time.Now()
-		fileName = currTime.Format("events.")
-		eventsLog.file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	path := filepath.Join(eventsLog.dir, "events.log.json")
+	if eventsLog.file != nil {
+		eventsLog.file.Close()
+		eventsLog.file = nil
+		newPath := eventsLog.currTime.Format("events.log-2006-01-02.json")
+		err = os.Rename(path, newPath)
+		// TODO: mark EventsLog as invalid so we don't keep logging on error?
 		if err != nil {
 			return err
 		}
-		eventsLog.enc = json.NewEncoder(eventsLog.file)
-	*/
+	}
+
+	eventsLog.currTime = time.Now()
+	eventsLog.file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		// TODO: mark EventsLog as invalid so we don't keep logging on error?
+		return err
+	}
+	eventsLog.enc = json.NewEncoder(eventsLog.file)
+	return nil
 }
 
+// InitEventsLogMust initializes event logging
 func InitEventsLogMust(dir string) error {
 	err := os.MkdirAll(dir, 0755)
 	u.PanicIfErr(err)
@@ -63,8 +66,14 @@ func InitEventsLogMust(dir string) error {
 	return reopenEventsFileIfNeeded()
 }
 
+// LogEvent logs another event as json
 func LogEvent(v interface{}) {
-	err := eventsLog.enc.Encode(v)
+	// TODO: do logging on a separate goroutine to prevent random stalls due to i/o
+	err := reopenEventsFileIfNeeded()
+	if err != nil {
+		return
+	}
+	err = eventsLog.enc.Encode(v)
 	if err != nil {
 		log.Printf("eventsLog.enc.Encode() failed with %s\n", err)
 	}
