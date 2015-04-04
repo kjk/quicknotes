e7aac13f3220f5d79b248cc8e8b8228dbc6af444
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-04 15:20:23 -0700

    wip for events

--- events.go
+++ events.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
-	"path/filepath"
+	"time"
 
 	"github.com/kjk/u"
 )
@@ -17,22 +17,50 @@ var (
 	eventsLog *EventsLog
 )
 
+// EventsLog describes a single log with events
 type EventsLog struct {
-	dir  string
-	file *os.File
-	enc  *json.Encoder
+	dir      string
+	file     *os.File
+	enc      *json.Encoder
+	currPath string
+	currTime time.Time
 }
 
-func InitEventsLogMust(dir string) {
+func reopenEventsFileIfNeeded() error {
+	if eventsLog.file != nil {
+		if eventsLog.currTime.YearDay() == time.Now().YearDay() {
+			return nil
+		}
+	}
+	/*
+		path := filepath.Join(dir, "events.log.json")
+		if eventsLog.file != nil {
+			eventsLog.file.Close()
+			eventsLog.file = nil
+			newPath := eventsLog.currTime.Format("events.log-2006-01-02.json")
+			err := os.Rename(path, newPath)
+			if err != nil {
+
+			}
+		}
+
+		eventsLog.currTime = time.Now()
+		fileName = currTime.Format("events.")
+		eventsLog.file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			return err
+		}
+		eventsLog.enc = json.NewEncoder(eventsLog.file)
+	*/
+}
+
+func InitEventsLogMust(dir string) error {
 	err := os.MkdirAll(dir, 0755)
 	u.PanicIfErr(err)
 	eventsLog = &EventsLog{
 		dir: dir,
 	}
-	path := filepath.Join(dir, "events.log.json")
-	eventsLog.file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
-	u.PanicIfErr(err)
-	eventsLog.enc = json.NewEncoder(eventsLog.file)
+	return reopenEventsFileIfNeeded()
 }
 
 func LogEvent(v interface{}) {
