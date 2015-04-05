a857c0c08439cfe6b7664c821d07d5a7b348401f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-04 20:50:52 -0700

    more work on editing

--- jsx/FullComposer.jsx
+++ jsx/FullComposer.jsx
@@ -2,23 +2,37 @@
 'use strict';
 
 var FullComposer = React.createClass({
+  handleSave: function(e) {
+      console.log("onSave");
+      e.preventDefault();
+      // TODO: get properties on the note before calling save
+      this.props.saveNoteCb(this.props.note);
+  },
+
+  handleCancel: function(e) {
+    console.log("onCancel");
+    e.preventDefault();
+    this.props.cancelNoteEditCb(this.props.note);
+  },
+
   render: function() {
+    var note = this.props.note;
     if (!this.props.note) {
       return (
         <div id="full-composer-wrapper" className="collapsed">
         </div>
       );
     }
-
+    var noteContent = note.Snippet;
     return (
       <div id="full-composer-wrapper">
         <div id="full-composer-top">
-          <textarea id="full-composer-textarea">{this.props.note.IDStr}</textarea>
+          <textarea id="full-composer-textarea">{noteContent}</textarea>
           <div id="full-composer-preview"></div>
         </div>
         <div id="full-composer-bottom">
-          <button>Save</button>
-          <button>Cancel</button>
+          <button onClick={this.handleSave}>Save</button>
+          <button onClick={this.handleCancel}>Cancel</button>
         </div>
       </div>
       );
