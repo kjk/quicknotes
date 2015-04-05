c9ca24530806e0436cedb2ba4d106d99c413d18e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-04 23:43:16 -0700

    add title and tags in full compose

--- jsx/FullComposer.jsx
+++ jsx/FullComposer.jsx
@@ -1,6 +1,20 @@
 /* jshint -W097,-W117 */
 'use strict';
 
+function tagsToText(tags) {
+  if (!tags) {
+    return "";
+  }
+  var s = "";
+  tags.forEach(function(tag) {
+    if (s != "") {
+      s += " ";
+    }
+    s += "#" + tag;
+  });
+  return s;
+}
+
 var FullComposer = React.createClass({
   handleSave: function(e) {
       console.log("onSave");
@@ -15,6 +29,28 @@ var FullComposer = React.createClass({
     this.props.cancelNoteEditCb(this.props.note);
   },
 
+  componentDidMount: function() {
+    var el = React.findDOMNode(this.refs.editArea);
+    // TODO: hacky, if there is no note, we render but collapsed
+    if (el) {
+      el.focus();
+    }
+  },
+
+  componentDidUpdate: function() {
+    var el = React.findDOMNode(this.refs.editArea);
+    // TODO: hacky, if there is no note, we render but collapsed
+    if (el) {
+      el.focus();
+    }
+  },
+
+  textChanged: function(e) {
+    var s = e.target.value;
+    console.log("s: " + s);
+    e.preventDefault();
+  },
+
   render: function() {
     var note = this.props.note;
     if (!this.props.note) {
@@ -24,12 +60,26 @@ var FullComposer = React.createClass({
       );
     }
     var content = note.Content;
-    //var title = note.Title;
-    //var tags = note.Tags;
+    var title = note.Title;
+    var tags = tagsToText(note.Tags);
     return (
       <div id="full-composer-wrapper">
+        <div id="full-composer-title">
+          <span>Title:</span>
+          <input type="text" defaultValue={title} size="80"/>
+        </div>
+
+        <div id="full-composer-tags">
+          <span>Tags:</span>
+          <input type="text" defaultValue={tags} size="80"/>
+        </div>
+
         <div id="full-composer-top">
-          <textarea id="full-composer-textarea">{content}</textarea>
+          <textarea
+            id="full-composer-textarea"
+            value={content}
+            onChange={this.textChanged}
+            ref="editArea"></textarea>
           <div id="full-composer-preview"></div>
         </div>
         <div id="full-composer-bottom">
