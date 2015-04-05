653c12728627cfb87d98109dd7ff58c456bf7d6a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-04 19:55:33 -0700

    more stuff for full composer

--- jsx/FullComposer.jsx
+++ jsx/FullComposer.jsx
@@ -1,21 +1,24 @@
-/* jshint -W097 */
+/* jshint -W097,-W117 */
 'use strict';
 
 var FullComposer = React.createClass({
   render: function() {
     if (!this.props.isShown) {
       return (
-        <div id="full-composer" className="collapsed">
+        <div id="full-composer-wrapper" className="collapsed">
         </div>
-      )
+      );
     }
 
     return (
-      <div id="full-composer">
-        <div className="inner">
-          <textarea
-            id="full-composer-textarea"
-          />
+      <div id="full-composer-wrapper">
+        <div id="full-composer-top">
+          <textarea id="full-composer-textarea"/>
+          <div id="full-composer-preview"></div>
+        </div>
+        <div id="full-composer-bottom">
+          <button>Save</button>
+          <button>Cancel</button>
         </div>
       </div>
       );
