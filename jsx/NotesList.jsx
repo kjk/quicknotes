f9fb92c02edbac434df0ee9d206cd10d43d6ca0c
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-05 17:25:13 -0700

    limit number of notes in initial render to 50 for much better performance

--- jsx/NotesList.jsx
+++ jsx/NotesList.jsx
@@ -3,13 +3,36 @@
 
 var Note = require('./Note.jsx');
 
+// TODO: implement infinite scroll by adding more notes on scroll
+// Note: rendering too many notes in a single render cycle causes noticeable
+// pause at startup and when filtering notes via tags. Empirically, 50 is
+// instantaneous. Around 100 there's a bit of a lag. At 800 it becomes bad.
+var maxInitialNotes = 50;
+function truncateNotes(notes) {
+  if (maxInitialNotes != -1 && notes.length >= maxInitialNotes) {
+    return notes.slice(0, maxInitialNotes);
+  }
+  return notes;
+}
+
 var NotesList = React.createClass({
+  getInitialState: function() {
+    return {
+      notes: truncateNotes(this.props.notes)
+    };
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      notes: truncateNotes(nextProps.notes)
+    });
+  },
 
   render: function () {
     var self = this;
     return (
       <div id="notes-list">
-        {this.props.notes.map(function(note) {
+        {this.state.notes.map(function(note) {
           return <Note
             compact={self.props.compact}
             note={note}
