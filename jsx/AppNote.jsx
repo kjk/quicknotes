4e570773e0cc793f56420e0c5392dc196420e383
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-07 09:42:48 -0700

    forgot to add the files

---
+++ jsx/AppNote.jsx
@@ -0,0 +1,17 @@
+/* jshint -W097,-W117 */
+'use strict';
+
+var Top = require("./Top.jsx");
+
+function appNoteStart() {
+  console.log("appNoteStart: gLoggedInUserHandle: ", gLoggedInUserHandle);
+  var isLoggedIn = gLoggedInUserHandle != "";
+  React.render(
+    <Top isLoggedIn={isLoggedIn}
+      loggedInUserHandle={gLoggedInUserHandle}
+      notesUserHandle="" />,
+    document.getElementById('note-top')
+  );
+}
+
+window.appNoteStart = appNoteStart;
