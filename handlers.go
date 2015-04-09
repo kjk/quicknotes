257fc1321884ee3f16800f94422f33eefdba3b08
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-09 00:01:33 -0700

    progress on permanent delete of notes

--- handlers.go
+++ handlers.go
@@ -434,7 +434,30 @@ func handleAPIDeleteNote(w http.ResponseWriter, r *http.Request) {
 	httpOkWithJSON(w, v)
 }
 
-// GET /api/undeletenote.json
+// POST /api/permanentdeletenote.json
+// args:
+// - noteIdHash
+func handleAPIPermanentDeleteNote(w http.ResponseWriter, r *http.Request) {
+	LogInfof("url: '%s'\n", r.URL)
+	dbUser, noteID := getUserNoteFromArgs(w, r)
+	if dbUser == nil {
+		return
+	}
+	err := dbPermanentDeleteNote(dbUser.ID, noteID)
+	if err != nil {
+		httpErrorWithJSONf(w, "failed to permanently delete note with '%s'", err)
+		return
+	}
+	LogInfof("permanently deleted note %d\n", noteID)
+	v := struct {
+		Msg string
+	}{
+		Msg: "note has been permanently deleted",
+	}
+	httpOkWithJSON(w, v)
+}
+
+// POST /api/undeletenote.json
 // args:
 // - noteIdHash
 func handleAPIUndeleteNote(w http.ResponseWriter, r *http.Request) {
@@ -649,6 +672,7 @@ func registerHTTPHandlers() {
 	http.HandleFunc("/api/searchusernotes.json", handleSearchUserNotes)
 	http.HandleFunc("/api/createorupdatenote.json", handleAPICreateOrUpdateNote)
 	http.HandleFunc("/api/deletenote.json", handleAPIDeleteNote)
+	http.HandleFunc("/api/permanentdeletenote.json", handleAPIPermanentDeleteNote)
 	http.HandleFunc("/api/undeletenote.json", handleAPIUndeleteNote)
 	http.HandleFunc("/api/makenoteprivate.json", handleAPIMakeNotePrivate)
 	http.HandleFunc("/api/makenotepublic.json", handleAPIMakeNotePublic)
