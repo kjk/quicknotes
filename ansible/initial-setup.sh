cec51427c2076c000e01d63f85aad4c539dfbc19
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-23 00:05:42 -0700

    deploy to gce

--- ansible/initial-setup.sh
+++ ansible/initial-setup.sh
@@ -4,7 +4,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+
 # ansible_ssh_private_key_file=$HOME/.ssh/id_rsa_apptranslator
-#ansible sumatrawebsite -m ping
+#ansible quicknotes-initial -m ping
 cd ansible
 ansible-playbook initial-setup.yml
