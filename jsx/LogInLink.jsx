16ec93946f25132d087cbbc314d2caf77639ab88
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-05-25 00:50:30 -0700

    tweak log in style

--- jsx/LogInLink.jsx
+++ jsx/LogInLink.jsx
@@ -21,14 +21,13 @@ var LogInLink = React.createClass({
         </a>
         <div className="dropdown">
           <i className="fa fa-chevron-down"></i>
-
           <div className="dropdown-content">
             <a href={userUrl}>My notes</a>
             <span className="divider"></span>
             <a href="#" onClick={this.handleSettings}>Settings</a>
             <a href="/import">Import notes</a>
             <span className="divider"></span>
-            <a href={url}>Log Out</a>
+            <a href={url}>Sign Out</a>
           </div>
         </div>
       </div>
@@ -40,15 +39,16 @@ var LogInLink = React.createClass({
     return (
       <div id="user">
         <span className="user">
-          Log in
+          Sign in / Sign up
         </span>
         <div className="dropdown">
           <i className="fa fa-chevron-down"></i>
-
-          <div className="dropdown-content">
-            <a href={twitterUrl}>with Twitter</a>
-            <a href="/logingoogle?redir=%2F">with Google</a>
-            <a href="/logingithub?redir=%2F">with GitHub</a>
+          <div className="log-in-dropdown-content">
+            <ul className="log-in">
+              <li><a className="twitter" href={twitterUrl}>Sign in with Twitter</a></li>
+              <li><a className="google" href="/logingoogle?redir=%2F">Sign in with Google</a></li>
+              <li><a className="github" href="/logingithub?redir=%2F">Sign in with GitHub</a></li>
+            </ul>
           </div>
         </div>
       </div>
