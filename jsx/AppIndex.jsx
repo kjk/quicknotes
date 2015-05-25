f58502ef066671a7d14d7ef27a2a5f459cda285b
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-05-24 22:27:23 -0700

    show top nav on index page

--- jsx/AppIndex.jsx
+++ jsx/AppIndex.jsx
@@ -1,22 +1,68 @@
 /* jshint -W097,-W117 */
 'use strict';
 
+var action = require('./action.js');
 var Top = require('./Top.jsx');
+var Settings = require('./Settings.jsx');
+
+var AppIndex = React.createClass({
+
+  getInitialState: function() {
+    return {
+      showingSettings: false
+    };
+  },
+
+  showSettings: function() {
+    console.log("showSettings");
+    this.setState({
+      showingSettings: true
+    });
+  },
+
+  hideSettings: function() {
+    console.log("hideSettings");
+    this.setState({
+      showingSettings: false
+    });
+  },
+
+  componentDidMount: function() {
+    this.cidShowSettings = action.onShowSettings(this.showSettings);
+    this.cidHideSettings = action.onHideSettings(this.hideSettings);
+  },
+
+  componentWillUnmount: function() {
+    action.onShowSettings(this.cidShowSettings)
+    action.onHideSettings(this.cidHideSettings)
+  },
+
+  renderSettings: function() {
+    console.log("renderSettings: ", this.state.showingSettings);
+    if (this.state.showingSettings) {
+      return <Settings />;
+    }
+  },
+
+  render: function() {
+    console.log("AppIndex: gLoggedInUserHandle: ", gLoggedInUserHandle);
+    var isLoggedIn = gLoggedInUserHandle !== "";
+    return (
+      <div>
+        <Top isLoggedIn={isLoggedIn}
+          loggedInUserHandle={gLoggedInUserHandle}
+          notesUserHandle="" />
+        {this.renderSettings()}
+      </div>
+    );
+  }
+});
 
 function appIndexStart() {
-  console.log("appIndexStart: gLoggedInUserHandle: ", gLoggedInUserHandle);
-  var isLoggedIn = gLoggedInUserHandle !== "";
   React.render(
-    <Top isLoggedIn={isLoggedIn}
-      loggedInUserHandle={gLoggedInUserHandle}
-      notesUserHandle="" />,
-    document.getElementById('header')
+    <AppIndex />,
+    document.getElementById('page-content')
   );
 }
 
-function doNothing() {
-
-}
-
-//window.appIndexStart = appIndexStar;
-window.appIndexStart = doNothing;
+window.appIndexStart = appIndexStart;
