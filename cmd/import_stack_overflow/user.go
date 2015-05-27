2c853aeeb0c4b78ff2dc0dc01d8c0d5a2342fc8d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-05-26 18:38:30 -0700

    remove duplication from reader

--- cmd/import_stack_overflow/user.go
+++ cmd/import_stack_overflow/user.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"encoding/xml"
-	"errors"
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -77,101 +75,4 @@ func decodeUserRow(t xml.Token, u *User) error {
 		}
 	}
 	return nil
-}
-
-// UserReader is for iteratively reading User
-type UserReader struct {
-	f        *os.File
-	d        *xml.Decoder
-	User     User
-	err      error
-	finished bool
-}
-
-// NewUserReader returns a new reader for User.xml file
-func NewUserReader(path string) (*UserReader, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		if f != nil {
-			f.Close()
-		}
-	}()
-
-	r := &UserReader{
-		f: f,
-		d: xml.NewDecoder(f),
-	}
-	t, err := getTokenIgnoreCharData(r.d)
-	if err != nil {
-		return nil, err
-	}
-	// skip <?xml ...>
-	if isProcInst(t) {
-		t, err = getTokenIgnoreCharData(r.d)
-		if err != nil {
-			return nil, err
-		}
-	}
-	if !isStartElement(t, "users") {
-		fmt.Printf("NewUserReader: invalid first token: %#v\n", t)
-		f.Close()
-		return nil, errors.New("invalid first token")
-	}
-	r.Next()
-	if r.err != nil {
-		return nil, r.err
-	}
-	f = nil
-	return r, nil
-}
-
-// Err returns potential error
-func (r *UserReader) Err() error {
-	return r.err
-}
-
-// Next advances to next User record. Returns false on end or
-func (r *UserReader) Next() bool {
-	if r.err != nil || r.finished {
-		return false
-	}
-
-	defer func() {
-		if r.err != nil {
-			r.f.Close()
-			r.f = nil
-		}
-	}()
-
-	// skip newlines between eleemnts
-	t, err := getTokenIgnoreCharData(r.d)
-	if err != nil {
-		r.err = err
-		return false
-	}
-
-	if isEndElement(t, "row") {
-		t, r.err = getTokenIgnoreCharData(r.d)
-		if r.err != nil {
-			return false
-		}
-	}
-
-	if isEndElement(t, "users") {
-		r.finished = true
-		return false
-	}
-
-	if !isStartElement(t, "row") {
-		r.err = fmt.Errorf("unexpected token: %#v, wanted xml.StartElement 'row'", t)
-		return false
-	}
-	r.err = decodeUserRow(t, &r.User)
-	if r.err != nil {
-		return false
-	}
-	return true
 }
