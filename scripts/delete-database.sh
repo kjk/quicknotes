16ff740f3bc32ca7dcbdda5dfab0635e11d05166
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-05 23:06:00 -0800

    use gdep

--- scripts/delete-database.sh
+++ scripts/delete-database.sh
@@ -4,8 +4,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-godep go tool vet .
-godep go build -o quicknotes
-#godep go build -race -o quicknotes
+gdep go tool vet .
+gdep go build -o quicknotes
+#gdep go build -race -o quicknotes
 ./quicknotes -local -deldb || true
 rm quicknotes
