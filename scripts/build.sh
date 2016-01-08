d8c3b6928765e3c93f80c452b0ef97d5787a1fd7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-08 02:47:43 -0800

    progress on updating the build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,14 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+echo "running fmt"
+./node_modules/.bin/esformatter -i js/*js* *.js
+
+. scripts/lint.sh
+
+echo "running gulp default"
+./node_modules/.bin/gulp default
+
 gdep go build -o quicknotes
 #gdep go build -race
 rm quicknotes
