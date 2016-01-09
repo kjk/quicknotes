dd84c1a515c8999c945580736dc2eb6dd359807e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-08 18:31:09 -0800

    add ability to blacklist files from zippped resources

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -4,19 +4,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-#. scripts/fmt.sh
-. scripts/lint.sh
-
-rm -rf s/dist/*.map s/dist/*.js s/dist/*.css
-
-echo "running gulp prod"
-./node_modules/.bin/gulp prod
-
-echo "running tools/gen_resources.go"
-go run tools/gen_resources.go
-
-echo "go build"
-gdep go build -tags embedded_resources -o quicknotes
+. scripts/build_production.sh
 
 #go build -o quicknotes
 #gdep go build -race -o quicknotes
