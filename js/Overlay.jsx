2bc46cd7354de4dbe52b471bb3e323c4ba7df20d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-14 16:09:15 -0800

    dim stuff behind editor

--- js/Overlay.jsx
+++ js/Overlay.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const Overlay = (props) => {
-  return <div className="overlay"></div>;
+  return (
+    <div className="overlay">
+      { props.children }
+    </div>
+    );
 };
