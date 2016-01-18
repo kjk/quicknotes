3a4692969faf1a5c0ee4f2c0b3108557056a4dca
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-18 00:47:56 -0800

    CodeMirror simplifications

--- js/CodeMirrorEditor.jsx
+++ js/CodeMirrorEditor.jsx
@@ -1,21 +1,14 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import CodeMirror from 'codemirror';
 
-// adapted from:
-// https://github.com/facebook/react/blob/master/docs/_js/live_editor.js#L16
-
-// also used as an example:
-// https://github.com/facebook/react/blob/master/src/browser/ui/dom/components/ReactDOMInput.js
-
-const isMobile = typeof navigator === 'undefined' || (
-  navigator.userAgent.match(/Android/i)
-  || navigator.userAgent.match(/webOS/i)
-  || navigator.userAgent.match(/iPhone/i)
-  || navigator.userAgent.match(/iPad/i)
-  || navigator.userAgent.match(/iPod/i)
-  || navigator.userAgent.match(/BlackBerry/i)
-  || navigator.userAgent.match(/Windows Phone/i)
-);
+// some references
+// https://github.com/facebook/react/blob/master/docs/_js/live_editor.js
+// https://github.com/Live4Code/react-codemirror/blob/master/codemirror/app/components/CodeMirror.jsx
+//https://github.com/yuyu1911/wangzhe_learning/blob/master/src/javascripts/react-codemirror.jsx
+// https://github.com/JedWatson/react-codemirror/blob/master/src/Codemirror.js
+// https://github.com/JedWatson/react-md-editor
+// https://github.com/ForbesLindesay/react-code-mirror/blob/master/standalone.js
 
 export default class CodeMirrorEditor extends Component {
 
@@ -23,84 +16,59 @@ export default class CodeMirrorEditor extends Component {
     super(props, context);
 
     this.handleChange = this.handleChange.bind(this);
-
-    this.state = {
-      isControlled: this.props.value != null
-    };
   }
 
   componentDidMount() {
-    const isTextArea = this.props.forceTextArea || isMobile;
-    if (isTextArea) {
-      return;
-    }
-    this.editor = CodeMirror.fromTextArea(this.editorNode, this.props);
-    this.editor.on('change', this.handleChange);
-    this.props.onEditorCreated && this.props.onEditorCreated(this.editor);
+    const node = ReactDOM.findDOMNode(this.refs.editorNode);
+    this.cm = CodeMirror.fromTextArea(node, this.props);
+    this.cm.on('change', this.handleChange);
+    this.props.onEditorCreated && this.props.onEditorCreated(this.cm);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return false;
   }
 
   componentDidUpdate() {
-    if (!this.editor || !this.props.value) {
-      return;
-    }
-    if (this.editor.getValue() !== this.props.value) {
-      this.editor.setValue(this.props.value);
+    if (this.cm && this.props.readOnly) {
+      this.cm.setValue(this.props.value);
     }
   }
 
+  componentWillUnmount() {
+    this.cm.off('change', this.handleChange);
+  }
+
   handleChange() {
-    if (!this.editor) {
-      return;
-    }
-    var value = this.editor.getValue();
-    if (value === this.props.value) {
-      return;
-    }
-
-    this.props.onChange && this.props.onChange({
-      target: {
-        value: value
-      }
-    });
-
-    if (this.editor.getValue() !== this.props.value) {
-      if (this.state.isControlled) {
-        this.editor.setValue(this.props.value);
-      } else {
-        this.props.value = value;
-      }
+    if (!this.props.readOnly && this.props.onChange) {
+      this.props.onChange(this.cm);
     }
   }
 
   render() {
-    const setEditorNode = n => this.editorNode = n;
-    const editor = React.createElement('textarea', {
-      ref: setEditorNode,
-      value: this.props.value,
-      readOnly: this.props.readOnly,
-      defaultValue: this.props.defaultValue,
-      onChange: this.props.onChange,
-      style: this.props.textAreaStyle,
-      className: this.props.textAreaClassName || this.props.textAreaClass
-    });
+    console.log('CodeMirrorEditor.render');
+    const editor = (
+    <textarea ref="editorNode"
+      defaultValue={ this.props.defaultValue }
+      style={ this.props.textAreaStyle }
+      className={ this.props.textAreaClassNames } />
+    );
 
-    return React.createElement('div', {
-      style: this.props.style,
-      className: this.props.className
-    }, editor);
+    return (
+      <div style={ this.props.style } className={ this.props.className }>
+        { editor }
+      </div>
+      );
   }
 }
 
 CodeMirrorEditor.propTypes = {
-  value: PropTypes.string,
   defaultValue: PropTypes.string,
   style: PropTypes.object,
   className: PropTypes.string,
   onChange: PropTypes.func,
   onEditorCreated: PropTypes.func,
-  forceTextArea: PropTypes.bool,
   readOnly: PropTypes.bool,
   textAreaStyle: PropTypes.object,
   textAreaClassName: PropTypes.string,
-  textAreaClass: PropTypes.string
 };
