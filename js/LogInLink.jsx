702d3d65a10bc383f08f23d940b630a41b3282c0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-18 18:40:38 -0800

    disable 'Settings' menu item

--- js/LogInLink.jsx
+++ js/LogInLink.jsx
@@ -26,8 +26,13 @@ const LinkLoggedIn = (props) => {
         <i className="fa fa-chevron-down"></i>
         <div className="dropdown-content">
           <a href={ userUrl }>My notes</a>
+          {false ?
           <a href="#" onClick={ showSettings }>Settings</a>
+          :null}
+          {false ?
           <span className="divider"></span>
+          :null
+          }
           <a href="/import" onClick={ showImportSimpleNote }>Import from Simplenote</a>
           <span className="divider"></span>
           <a href={ url }>Sign Out</a>
