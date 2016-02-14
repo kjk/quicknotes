8d1a581e0ce8b827763b2faee1f11960be8507b0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-02-13 22:12:38 -0800

    add target=_blank to rendered links

--- js/md.js
+++ js/md.js
@@ -55,6 +55,26 @@ const markdownItOpts = {
 
 const markdownIt = new MarkdownIt(markdownItOpts);
 
+// https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md
+// Remember old renderer, if overriden, or proxy to default renderer
+var defaultRender = markdownIt.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+  return self.renderToken(tokens, idx, options);
+};
+
+markdownIt.renderer.rules.link_open = function(tokens, idx, options, env, self) {
+  // If you are sure other plugins can't add `target` - drop check below
+  var aIndex = tokens[idx].attrIndex('target');
+
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']); // add new attribute
+  } else {
+    tokens[idx].attrs[aIndex][1] = '_blank'; // replace value of existing attr
+  }
+
+  // pass token to default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
+
 function toHtmlMarkdownIt(s) {
   s = s.trim();
   const html = markdownIt.render(s);
