e4691fcd7f65c6907297c88952b05bc21ef4ad8e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-02-19 05:18:08 -0800

    send e-mail when starting up

--- send_mail.go
+++ send_mail.go
@@ -9,6 +9,10 @@ import (
 	"github.com/mostafah/mandrill"
 )
 
+func init() {
+	mandrill.Key = "xmSus9bPNB7qI5_G7_Iibg"
+}
+
 func getStatsEmailBody() string {
 	nUsers, _ := dbGetUsersCount()
 	nNotes, _ := dbGetNotesCount()
@@ -22,20 +26,32 @@ func getStatsEmailBody() string {
 	return strings.Join(a, "\n")
 }
 
-func sendStatsMail() {
-	mandrill.Key = "xmSus9bPNB7qI5_G7_Iibg"
+func sendMail(subject, body, from string) {
 	err := mandrill.Ping()
 	if err != nil {
 		log.Errorf("mandrill.Ping() failed with %s\n", err)
 		return
 	}
 	msg := mandrill.NewMessageTo("kkowalczyk@gmail.com", "Krzysztof Kowalczyk")
-	msg.Text = getStatsEmailBody()
-	msg.Subject = time.Now().Format("Quicknotes stats on 2006-01-02")
+	msg.Subject = subject
+	msg.Text = body
 	msg.FromEmail = "info@quicknotes.io"
-	msg.FromName = "QuickNotes Stats"
+	msg.FromName = from
 	_, err = msg.Send(false)
 	if err != nil {
 		log.Errorf("msg.Send() failed with %s\n", err)
 	}
+}
+
+func sendStatsMail() {
+	subject := time.Now().Format("QuickNotes stats on 2006-01-02")
+	body := getStatsEmailBody()
+	sendMail(subject, body, "QuickNotes Stats")
+}
+
+func sendBootMail() {
+	subject := time.Now().Format("QuickNotes started on 2006-01-02")
+	body := "Just letting you know that I've started\n"
+	body += fmt.Sprintf("local: %v, proddb: %v, sql connection: %s, data dir: %s\n", flgIsLocal, flgProdDb, getSQLConnectionRoot(), getDataDir())
+	sendMail(subject, body, "QuickNotes")
 }
