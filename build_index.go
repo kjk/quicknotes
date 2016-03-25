4af5e2c2aa6847880d5375544a98564cd993589f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-03-24 23:21:32 -0700

    improve generation of /idx/allnotes

--- build_index.go
+++ build_index.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/kjk/log"
@@ -11,11 +16,17 @@ const (
 	nNotesPerPage = 100
 )
 
+var (
+	buildIndexMu sync.Mutex
+)
+
 // NoteIndex represents a note on the notes index page
 type NoteIndex struct {
-	URL       string
-	Title     string
-	CreatedAt time.Time
+	URL             string
+	Title           string
+	Creator         string
+	CreatedAt       time.Time
+	CreatedAtString string
 }
 
 func notesIndexPagePath(pageNo int) string {
@@ -23,21 +34,43 @@ func notesIndexPagePath(pageNo int) string {
 	return pathForFileInCache(name)
 }
 
-func buildNotesIndexPage(pageNo int, notes []NoteIndex) error {
+func deleteIndexPages() {
+	d := getCacheDir()
+	files, err := ioutil.ReadDir(d)
+	if err != nil {
+		log.Errorf("ioutil.Read('%s') failed with '%s'\n", d, err)
+		return
+	}
+	for _, file := range files {
+		name := file.Name()
+		if strings.HasPrefix(name, "public-notes-index-") {
+			path := filepath.Join(d, name)
+			err = os.Remove(path)
+			if err != nil {
+				log.Errorf("os.Remove('%s') failed with '%s'\n", path, err)
+			}
+		}
+	}
+}
+
+func buildNotesIndexPage(nNotes, nPages, pageNo int, notes []NoteIndex) error {
 	if len(notes) == 0 {
 		return nil
 	}
 	v := struct {
+		NumNotes   int
+		NumPages   int
 		PageNo     int
 		Notes      []NoteIndex
 		NextPageNo int
 	}{
+		NumNotes:   nNotes,
+		NumPages:   nPages,
 		PageNo:     pageNo,
 		Notes:      notes,
 		NextPageNo: 0,
 	}
-	// doesn't work if number of notes is exactly nNotesPerPage * N
-	if len(notes) == nNotesPerPage {
+	if pageNo != nPages {
 		v.NextPageNo = pageNo + 1
 	}
 	path := notesIndexPagePath(pageNo)
@@ -52,30 +85,56 @@ func buildNotesIndexPage(pageNo int, notes []NoteIndex) error {
 // unconditionally generate index page for public notes
 func buildPublicNotesIndex() error {
 	log.Verbosef("buildPublicNotesIndex\n")
+
+	buildIndexMu.Lock()
+	defer buildIndexMu.Unlock()
+
 	timeStart := time.Now()
+	deleteIndexPages()
 	pageNo := 1
 	nNotes := 0
 	db := getDbMust()
+
 	q := `
+SELECT count(*)
+FROM notes
+WHERE is_public = true AND is_deleted = false AND is_encrypted = false`
+
+	row := db.QueryRow(q)
+	err := row.Scan(&nNotes)
+	if err != nil {
+		log.Errorf("row.Scan() for '%s' failed with '%s'\n", q, err)
+		return err
+	}
+
+	nPages := nNotes / nNotesPerPage
+	if nNotes%nNotesPerPage != 0 {
+		nPages++
+	}
+
+	q = `
 SELECT
   id,
+  user_id,
   created_at,
   title
 FROM notes
 WHERE is_public = true AND is_deleted = false AND is_encrypted = false
-ORDER BY id DESC`
+ORDER BY created_at DESC`
 	rows, err := db.Query(q)
 	if err != nil {
-		log.Errorf("db.Query('%s') failed with %s\n", q, err)
+		log.Errorf("db.Query('%s') failed with '%s'\n", q, err)
 		return err
 	}
 	var notes []NoteIndex
 	for rows.Next() {
 		var noteID int
+		var userID int
 		var createdAt time.Time
 		var title string
 		err := rows.Scan(
 			&noteID,
+			&userID,
 			&createdAt,
 			&title)
 		if err != nil {
@@ -87,13 +146,20 @@ ORDER BY id DESC`
 		} else {
 			url = url + "-" + title
 		}
+		userInfo, err := getCachedUserInfo(userID)
+		creator := "unknown"
+		if userInfo != nil {
+			creator = userInfo.user.GetHandle()
+		}
 		notes = append(notes, NoteIndex{
-			URL:       url,
-			Title:     title,
-			CreatedAt: createdAt,
+			URL:             url,
+			Title:           title,
+			Creator:         creator,
+			CreatedAt:       createdAt,
+			CreatedAtString: createdAt.Format("2006-01-02"),
 		})
 		if len(notes) == nNotesPerPage {
-			err = buildNotesIndexPage(pageNo, notes)
+			err = buildNotesIndexPage(nNotes, nPages, pageNo, notes)
 			if err != nil {
 				log.Errorf("buildNotesIndexPage() failed with '%s'\n", err)
 				return err
@@ -103,7 +169,7 @@ ORDER BY id DESC`
 		}
 		nNotes++
 	}
-	err = buildNotesIndexPage(pageNo, notes)
+	err = buildNotesIndexPage(nNotes, nPages, pageNo, notes)
 	if err != nil {
 		log.Errorf("buildNotesIndexPage() failed with '%s'\n", err)
 		return err
