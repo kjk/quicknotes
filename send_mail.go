6f841063b4e80a8bb92b2eaff1a106636c334b40
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-03-24 19:37:25 -0700

    change e-mail sending from mandrill to sparkpost

--- send_mail.go
+++ send_mail.go
@@ -5,13 +5,13 @@ import (
 	"strings"
 	"time"
 
+	sp "github.com/SparkPost/gosparkpost"
 	"github.com/kjk/log"
-	"github.com/mostafah/mandrill"
 )
 
-func init() {
-	mandrill.Key = "xmSus9bPNB7qI5_G7_Iibg"
-}
+const (
+	sparkpostKey = "0f6d54023ed5e6e4beb0c55e8f910064a5605151"
+)
 
 func getStatsEmailBody() string {
 	nUsers, _ := dbGetUsersCount()
@@ -27,19 +27,24 @@ func getStatsEmailBody() string {
 }
 
 func sendMail(subject, body, from string) {
-	err := mandrill.Ping()
+	var sparky sp.Client
+	err := sparky.Init(&sp.Config{ApiKey: sparkpostKey})
 	if err != nil {
-		log.Errorf("mandrill.Ping() failed with %s\n", err)
+		log.Errorf("sparky.Init() failed with: '%s'\n", err)
 		return
 	}
-	msg := mandrill.NewMessageTo("kkowalczyk@gmail.com", "Krzysztof Kowalczyk")
-	msg.Subject = subject
-	msg.Text = body
-	msg.FromEmail = "info@quicknotes.io"
-	msg.FromName = from
-	_, err = msg.Send(false)
+
+	tx := &sp.Transmission{
+		Recipients: []string{"kkowalczyk@gmail.com"},
+		Content: sp.Content{
+			Text:    subject,
+			From:    from,
+			Subject: subject,
+		},
+	}
+	_, _, err = sparky.Send(tx)
 	if err != nil {
-		log.Errorf("msg.Send() failed with %s\n", err)
+		log.Errorf("sparky.Send() failed with '%s'\n", err)
 	}
 }
 
@@ -54,4 +59,10 @@ func sendBootMail() {
 	body := "Just letting you know that I've started\n"
 	body += fmt.Sprintf("local: %v, proddb: %v, sql connection: %s, data dir: %s\n", flgIsLocal, flgProdDb, getSQLConnectionRoot(), getDataDir())
 	sendMail(subject, body, "QuickNotes")
+}
+
+func testSendEmail() {
+	subject := time.Now().Format("QuickNotes stats on 2006-01-02")
+	body := "this is a test e-mail"
+	sendMail(subject, body, "QuickNotes Stats <info@quicknotes.io>")
 }
