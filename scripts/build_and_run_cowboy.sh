548b614b7809071ac3f9c0909668027ecfd36c57
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-09-03 20:12:09 -0700

    add scripts/build_and_run_cowboy.sh

---
+++ scripts/build_and_run_cowboy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+. scripts/fmt.sh
+. scripts/lint.sh
+
+echo "running gulp default"
+./node_modules/.bin/gulp default
+
+echo "go build"
+gdep go build -race -o quicknotes
+
+#go build -o quicknotes
+#gdep go build -race -o quicknotes
+
+echo "starting quicknotes"
+./quicknotes -local -proddb || true
+rm quicknotes
