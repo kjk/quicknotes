acfb3afecb212724086646290b4af93d72f952bd
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-11-03 23:15:34 -0700

    start on electron app

---
+++ electron/main.js
@@ -0,0 +1,58 @@
+const { app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
+
+// TODO: login page specific to the app
+const startURL = 'https://quicknotes.io/';
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+    const mainWindowState = windowStateKeeper({
+        defaultWidth: 1024,
+        defaultHeight: 800,
+    });
+    mainWindow = new BrowserWindow({
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height,
+        webPreferences: {
+            allowDisplayingInsecureContent: true,
+        },
+    });
+
+    mainWindowState.manage(mainWindow);
+
+    mainWindow.loadURL(startURL);
+
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (true || process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
