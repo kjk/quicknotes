84243598bb0051add99d6a5ec27b2ef663d24318
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-11-06 20:40:02 -0800

    add menu for copy&paste

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+'use strict';
+
+const { app, BrowserWindow, Menu } = require('electron');
 const windowStateKeeper = require('electron-window-state');
 
 // TODO: login page specific to the app
@@ -11,6 +13,36 @@ let mainWindow;
 // https://github.com/sindresorhus/electron-is-dev
 const isDev = process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath);
 
+const iconPath = __dirname + '/icon.png';
+
+if (isDev) {
+    console.log('iconPath: ', iconPath);
+}
+
+function isMac() { return process.platform == 'darwin' }
+
+function createMainMenu() {
+  if (process.platform == 'darwin') { // To enable shortcuts on OSX
+
+    var template = [{
+          label: "Harmony",
+          submenu: [
+              { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+              { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+              { type: "separator" },
+              { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+              { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+              { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+              { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" },
+              { type: "separator" },
+              { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+          ]}
+      ];
+
+      Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  }
+}
+
 function createWindow() {
     const mainWindowState = windowStateKeeper({
         defaultWidth: 1024,
@@ -21,12 +53,15 @@ function createWindow() {
         y: mainWindowState.y,
         width: mainWindowState.width,
         height: mainWindowState.height,
+        icon: iconPath,
         webPreferences: {
             allowDisplayingInsecureContent: true,
         },
     });
 
     mainWindowState.manage(mainWindow);
+    createMainMenu();
+    app.setName('QuickNotes');
 
     mainWindow.loadURL(startURL);
 
@@ -47,7 +82,7 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (isDev || !isMac()) {
         app.quit();
     }
 });
