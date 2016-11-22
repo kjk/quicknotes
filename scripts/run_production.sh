3057ca116b4017dafa9c637b508c4d073b8e4351
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-11-21 21:59:40 -0800

    re-arrange how start scripts work

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -4,7 +4,11 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+IFS=\; read -a ip_port <<<"`./scripts/start_docker.py`"
+ip="${ip_port[0]}"
+port="${ip_port[1]}"
+
 . scripts/build_production.sh
 
 echo "starting quicknotes, using mysql from docker"
-./quicknotes_prod -local $@ || true
+./quicknotes_prod -local -verbose -db-host ${ip} -db-port ${port} $@ || true
