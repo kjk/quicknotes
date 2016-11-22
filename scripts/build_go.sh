d485a1193629c2b2f33fc3cb1316efc7c938d831
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-11-21 22:04:01 -0800

    less scripts

--- scripts/build_go.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o nounset
-set -o errexit
-set -o pipefail
-
-go vet github.com/kjk/quicknotes
-go build -o quicknotes
--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -4,11 +4,19 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+. scripts/lint.sh
+
+rm -rf s/dist/*.map s/dist/*.js s/dist/*.css tsbuild/ resources.go
+
+./node_modules/.bin/gulp prod
+
+go run tools/gen_resources.go
+
+go build -o quicknotes_prod -tags embeded_resources
+
 IFS=\; read -a ip_port <<<"`./scripts/start_docker.py`"
 ip="${ip_port[0]}"
 port="${ip_port[1]}"
-
-. scripts/build_production.sh
 
 echo "starting quicknotes, using mysql from docker"
 ./quicknotes_prod -local -verbose -db-host ${ip} -db-port ${port} $@ || true
