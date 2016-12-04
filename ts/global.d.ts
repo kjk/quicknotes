9c165876f2ff78b00e19a516963c1d0d362bb607
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-12-04 12:29:18 -0800

    rename ts/vendor.d.ts => ts/global.d.ts

--- ts/global.d.ts
+++ ts/global.d.ts
@@ -4,7 +4,6 @@ interface UserInfo {
   Handle: string;
 }
 
-declare var gRecentNotesInitial: any;
 declare var gNotesUser: UserInfo;
 declare var gLoggedUser: UserInfo;
 declare var gInitialNote: any;
