068a2be17e0b956cf289d48322921270cc74ae5d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-12-04 23:09:03 -0800

    reduce bundle size by excluding most languages from highlight.js

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -9,3 +9,7 @@ rm -rf typings
 
 yarn
 ./node_modules/.bin/typings install
+
+# this patches highlight.js in node_modules to only include a subset of
+# languages for a significant saving in final bundle (1.3 MB => 879 K)
+cp ./scripts/highlight_index_override.js ./node_modules/highlight.js/lib/index.js
