0096cb3b108ae81a53429c190bca1e8a0c2e9a24
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-06-11 21:16:57 -0700

    we don't have user in anonymous access

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,18 +5,14 @@ IFS=\; read -a ip_port <<<"`./scripts/start_docker.py`"
 ip="${ip_port[0]}"
 port="${ip_port[1]}"
 
-. scripts/fmt.sh
-. scripts/lint.sh
+# . scripts/fmt.sh
+# . scripts/lint.sh
 
 #echo "running gulp default"
 #./node_modules/.bin/gulp default
 
-echo "go build"
 go build -race -o quicknotes
-
 #go build -o quicknotes
-#go build -race -o quicknotes
 
-echo "starting quicknotes, using mysql from docker"
 ./quicknotes -verbose -db-host ${ip} -db-port ${port} $@ || true
 rm ./quicknotes
