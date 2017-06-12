2a1a3760b01f340ca7d661e9b9fd5fe2346074d4
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-06-11 20:15:49 -0700

    tweak urlifyTitle() to remove more bad chars and allow for longer titles

--- ts/NoteView.tsx
+++ ts/NoteView.tsx
@@ -6,8 +6,11 @@ import * as action from './action';
 import * as api from './api';
 
 function urlifyTitle(s: string) {
-  s = s.slice(0, 32);
-  return s.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-');
+  s = s.toLowerCase();
+  s = s.replace(/[^\w]+/g, '');
+  s = s.replace(/ +/g, '-');
+  s = s.replace(/\+:&#_^\$\*#/g, '');
+  return s.slice(0, 128);
 }
 
 interface State {
