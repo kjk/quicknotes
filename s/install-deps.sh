4fc25e3a1df2bcc7e3f7430c3103eb71e9510572
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-06-23 12:19:28 -0700

    don't pull marked and markdown-it anymore (settle on showdown.js)

--- s/install-deps.sh
+++ s/install-deps.sh
@@ -7,10 +7,8 @@ dep ensure
 # TODO: ideally we should delete to be extra sure it's clean
 # but sass compiler takes forever to install (builds C++)
 #rm -rf node_modules
-rm -rf typings
 
 yarn
-./node_modules/.bin/typings install
 
 # this patches highlight.js in node_modules to only include a subset of
 # languages for a significant saving in final bundle (1.3 MB => 879 K)
