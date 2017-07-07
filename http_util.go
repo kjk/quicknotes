5580a7c8280387508994bcb03c1325b05edfece0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-07-07 14:00:55 -0700

    use library function

--- http_util.go
+++ http_util.go
@@ -119,35 +119,6 @@ func getReferer(r *http.Request) string {
 	return r.Header.Get("Referer")
 }
 
-// Request.RemoteAddress contains port, which we want to remove i.e.:
-// "[::1]:58292" => "[::1]"
-func ipAddrFromRemoteAddr(s string) string {
-	idx := strings.LastIndex(s, ":")
-	if idx == -1 {
-		return s
-	}
-	return s[:idx]
-}
-
-func getIPAddress(r *http.Request) string {
-	hdr := r.Header
-	hdrRealIP := hdr.Get("X-Real-Ip")
-	hdrForwardedFor := hdr.Get("X-Forwarded-For")
-	if hdrRealIP == "" && hdrForwardedFor == "" {
-		return ipAddrFromRemoteAddr(r.RemoteAddr)
-	}
-	if hdrForwardedFor != "" {
-		// X-Forwarded-For is potentially a list of addresses separated with ","
-		parts := strings.Split(hdrForwardedFor, ",")
-		for i, p := range parts {
-			parts[i] = strings.TrimSpace(p)
-		}
-		// TODO: should return first non-local address
-		return parts[0]
-	}
-	return hdrRealIP
-}
-
 func dumpFormValueNames(r *http.Request) {
 	r.ParseForm()
 	r.ParseMultipartForm(128 * 1024)
