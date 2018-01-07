2e516a54d3541e0d0e40ee358959bd21b99c27a4
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-01-06 21:14:40 -0800

    move credentials to credentials.json

--- google_storage.go
+++ google_storage.go
@@ -17,18 +17,6 @@ import (
 
 const (
 	quicknotesBucket = "quicknotes"
-	credentialsJSON  = `{
-  "type": "service_account",
-  "project_id": "practical-truck-801",
-  "private_key_id": "8ee33f44d90312e8acacda70bbf7fbea98bed414",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDfcYt5sIhuUOHh\nYzcnh7nsq0iiaeJtaEqdWMPt1w7BDTHBEYKyly/2dYUHyxCvu9GwjbADBkXyu1nF\n7lSPHvVrPfdOt9DlNRfu60rqVCJ50IzFsXKopuG1q7MzdCZCHnf1rrGEOeGuUhBQ\nYRsNOrZjSmObs7rGGEXNKqaln9XubaCm1VIaUTS8yNXLuru6OQw/ssJ57JN9+4a4\nUaQY5XjXSjOi+HDxJduXIMUji/fDnkkksJluZEUlBEgZwQrdW9MduanV8/h4L5jz\nXQCE/mCzvtYcGGnTdeVm94U0e/mEpDr0pzp+pPKMxRabdTmwK+sqS7e5kYEgwnDJ\nJIh2obZHAgMBAAECggEBAIVuaLvij5ZQ9pKBjQ4uHvkVz+otEOoEvYn42AfPxR/D\noVKKAmJjpmCnDSn6OZy3rCCie9lShbLN7m4kRJqzAhtohaacXkKB0ij6mWIVnADi\naS4lKTNNdLvoLLstQSt6xmgQSjGL3xkaKGPXmS/tP71LTvBMA90H4acDMUD2keou\nr2vAESN9uocB4zt7zF85Mz7TIkPgxdeyOQeaoOOTxH2saTCHrJebWDMZVfCUxEuS\nnyv27278BbqIHk5wRFVl75hyBc6Vo4d/rHawacXl0Zx4uSNtBilFNo0PURnbneLm\n+6PcBrNpn6wIX2VwPGFj76SWTC9tP9DOnJ7T7h4IHQECgYEA9CJQLflsaOWoK32/\nc0DbfLvJvtIMANxy46hZlgsZX7hvGr8ZFlQ3RmFrBFKRPrLux+zVcgYZMgCUwozk\n2j9yseGHy3fWQs5pXD3r1ncawCcEuYliF67Hgsnlrt00edAVkLP7ZWlWohThctk5\n/D5Nvxob00Q2sX2WuAJdK1Imt4MCgYEA6k3JMN+yknSPieCDSmP6bkkMl37Uk++p\nma7ssixusCLuhOuBzl9W7klFf3F1dT4q7x8C9IDrEfK8Y7Oy1LAN8A4iLlMNQ5oN\n515igj3uW6sC0GfC0UPLsP1RsTslpI44OZEa6bR+H8OcZUpluGkTBN1Ie40NDZv1\noQkGfoQ0Ru0CgYARvEBo5prKkAyEhFEZNVf8msPQOgAnO9yYz00aylmgi4x+u+09\nowaU9VTJ7pmgnW0dVwY2j1zbNhGYhJXHiR5y77hmvqDxH3+l9NWiMerelLcSJ3nZ\n0Jer17D44BcE1moKphiYSGvDwJKUPlWpDnmlbWciFO2IPWBqo7lTAVfQswKBgDZA\nsrBda9TbpozvbpcgFVHKGl9N390tZmEOjwImpa6lOAORIKpviwp77tq2o9L1BS3/\nSUjiPQwX36Vaa7Mx3NhT/XgqhOy9VDDZuwebXYDeVTV6gTLBdv3RekQGhQ3YXI5/\n2fJ5d6bVPXZ6xpjvw5ahwnHRVE9taG/UTiunYw91AoGAbVWgvU9occo/940QuNMN\nkNyDyoUt6VZR+ZMTxGzT5P2JMXwFRPuV3P6hyM/tu7OQoWoZHbHTFeLLJpHe7VqZ\nYVKN3iaq/px0bafL6Db4RIbuHV6eAO2bK+2AIrXA4S6ih5ow1h4GpQdWf2SE0etn\nybXfeYPDOZVISk8hLUEvOR4=\n-----END PRIVATE KEY-----\n",
-  "client_email": "service-account-2@practical-truck-801.iam.gserviceaccount.com",
-  "client_id": "109176300735941648760",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://accounts.google.com/o/oauth2/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/service-account-2%40practical-truck-801.iam.gserviceaccount.com"
-}`
 )
 
 var (
@@ -37,11 +25,18 @@ var (
 	onlyLocalStorage bool
 )
 
+func loadGoogleStorageCredentialsMust() []byte {
+	d, err := ioutil.ReadFile("credentials.json")
+	u.PanicIfErr(err)
+	return d
+}
+
 func initGoogleStorageMust() {
 	// http://godoc.org/golang.org/x/oauth2/google
 	// another way: https://godoc.org/google.golang.org/cloud/storage#example-package--Auth
+	d := loadGoogleStorageCredentialsMust()
 	conf, err := google.JWTConfigFromJSON(
-		[]byte(credentialsJSON),
+		d,
 		storage.ScopeReadWrite,
 	)
 	u.PanicIfErr(err, "google.JWTConfigFromJSON")
--- localstore_test.go
+++ localstore_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/kjk/quicknotes/pkg/log"
+	"github.com/kjk/u"
 )
 
 // ContentID describes content path and its sha1
@@ -22,16 +23,16 @@ func TestLocalStore(t *testing.T) {
 		defer log.DecVerbosity()
 	}
 	log.LogToStdout = true
-	dir := ExpandTildeInPath("~/data/test_localstore")
+	dir := u.ExpandTildeInPath("~/data/test_localstore")
 	os.RemoveAll(dir)
-	CreateDirMust(dir)
+	u.CreateDirMust(dir)
 	store, err := NewLocalStore(dir)
-	PanicIfErr(err)
+	u.PanicIfErr(err)
 	store.MaxSegmentSize = 8192           // for testing
 	store.FileSizeSegmentThreshold = 7000 // localstore.go is bigger than that
 	var contentIds []*ContentID
 	files, err := ioutil.ReadDir(".")
-	PanicIfErr(err)
+	u.PanicIfErr(err)
 	for _, fi := range files {
 		path := fi.Name()
 		if !fi.Mode().IsRegular() {
@@ -39,22 +40,22 @@ func TestLocalStore(t *testing.T) {
 		}
 		fmt.Printf("file: %s, size: %d\n", path, fi.Size())
 		d, err := ioutil.ReadFile(path)
-		PanicIfErr(err)
+		u.PanicIfErr(err)
 		sha1, err := store.PutContent(d)
-		PanicIfErr(err)
+		u.PanicIfErr(err)
 		cid := &ContentID{sha1: sha1, path: path}
 		contentIds = append(contentIds, cid)
 	}
 
 	for _, cid := range contentIds {
 		d, err := store.GetContentBySha1(cid.sha1)
-		sha1 := Sha1OfBytes(d)
+		sha1 := u.Sha1OfBytes(d)
 		if !bytes.Equal(sha1, cid.sha1) {
 			t.Fatalf("invalid data for %s, sha1: %x cid.sha1: %x", cid.path, sha1, cid.sha1)
 		}
-		PanicIfErr(err)
+		u.PanicIfErr(err)
 		d2, err := ioutil.ReadFile(cid.path)
-		PanicIfErr(err)
+		u.PanicIfErr(err)
 		if !bytes.Equal(d, d2) {
 			t.Fatalf("invalid data for %s", cid.path)
 		}
